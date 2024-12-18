58ba45ed9e6b3cf58a23cb446737da1d25b92b8d
slinx <saaranshgarg1@gmail.com>
2024-12-18 09:14:27 +0530

    Update README.md
    fixes
    
    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
+// import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import '@tensorflow/tfjs-backend-webgl';
 import Webcam from "react-webcam";
